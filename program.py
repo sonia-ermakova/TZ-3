489514b5e188c6ef3d2766589ca60a394bf982fa
sonia-ermakova <43213893+sonia-ermakova@users.noreply.github.com>
2021-12-19 14:06:48 +0300

    Add files via upload

--- program.py
+++ program.py
@@ -3,8 +3,8 @@ def read_file():
     with open('file', 'r') as f:
         for line in f:
             info += line.strip("\n").split(' ')
-    data = [int(i) for i in info if i.isdigit()]
-    return data
+    ans = [int(el) for el in info if el.isdigit()]
+    return ans
 
 
 data = read_file()
@@ -12,34 +12,47 @@ data = read_file()
 
 def minimum(info):
     res = info[0]
-    for i in range(len(info)):
-        if info[i] <= res:
-            res = info[i]
+    for m in range(len(info)):
+        if info[m] <= res:
+            res = info[m]
     return res
 
 
 def maximum(info):
     res = info[0]
-    for i in range(len(info)):
-        if info[i] >= res:
-            res = info[i]
+    for s in range(len(info)):
+        if info[s] >= res:
+            res = info[s]
     return res
 
 
 def addition(info):
     res = 0
-    for i in range(len(info)):
-        res += info[i]
+    for l in range(len(info)):
+        res += info[l]
     return res
 
 
 def multiplication(info):
     res = 1
     try:
-        for i in range(len(info)):
-            res *= info[i]
-    except:
-        print('OverflowError')
+        for n in range(len(info)):
+            res *= info[n]
+    except OverflowError:
+        return 'Для корректной работы функции скорректируйт входные данные'
     else:
         return res
 
+
+ans_1, ans_2, ans_3 = '', '', ''
+for i in data:
+    ans_1 += str(i) + ' '
+    ans_2 += str(i) + '+'
+    ans_3 += str(i) + '*'
+print(
+    f'В файле: {ans_1}\n'
+    f'Минимальное: {minimum(data)}\n'
+    f'Максимальное: {maximum(data)}\n'
+    f'Сумма: {addition(data)} ({ans_2})\n'
+    f'Произведение: {multiplication(data)} ({ans_3})\n'
+)
